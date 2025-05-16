28a2d101499ae2899d1bf10ee5eed9c7feb49436
mrhrahimi <mr.hamedrhi@gmail.com>
2025-05-16 17:01:06 +0200

    Update global.R

--- global.R
+++ global.R
@@ -1,17 +1,37 @@
 
 source("libraries.R")
 
-url <- "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B"
+get_all_musees <- function(limit = 100) {
+  base_url <- "https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/liste-et-localisation-des-musees-de-france/records"
+  all_data <- list()
+  offset <- 0
 
-velib_data <- suppressWarnings(read_delim(url, delim = ";", escape_double = FALSE, trim_ws = TRUE))
+  repeat {
+    url <- paste0(base_url, "?limit=", limit, "&offset=", offset)
+    response <- GET(url)
+    if (response$status_code != 200) break
 
-velib_data <- velib_data %>%
-  rename(Commune = `Nom communes équipées`) %>%
-  separate(`Coordonnées géographiques`, into = c("latitude", "longitude"), sep = ",", convert = TRUE) %>%
-  mutate(
-    latitude = as.numeric(latitude),
-    longitude = as.numeric(longitude)
+    json_data <- content(response, as = "text", encoding = "UTF-8")
+    page_data <- fromJSON(json_data, flatten = TRUE)$results
+
+    if (length(page_data) == 0) break
+
+    all_data <- append(all_data, list(page_data))
+    offset <- offset + limit
+  }
+
+  bind_rows(all_data)
+}
+
+musees_raw <- get_all_musees()
+
+musees <- musees_raw %>%
+  rename(
+    nom = nom_officiel_du_musee,
+    adresse = adresse,
+    cp = code_postal,
+    ville = commune,
+    region = region_administrative,
+    departement = departement
   ) %>%
-  filter(!is.na(latitude), !is.na(longitude), !is.na(Commune))
-
-velib <- velib_data
+  filter(!is.na(latitude) & !is.na(longitude))
